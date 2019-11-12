da54c0b4b57c23f9ccb92c0d24344de4887b36bb
Ryunosuke Muramatsu <ryunosuke.muramatsu@mobilus.co.jp>
2019-11-12 15:59:03 +0900

    Initial Commit

---
+++ MVP-Sampler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP-Sampler
+//
+//  Created by Muramatsu Ryunosuke on 2019/11/12.
+//  Copyright © 2019 Muramatsu Ryunosuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
